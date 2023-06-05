d4dc66d7d2efa7f3642cbb21b6a7b54f6ac3bd6c
Alla Yevlampiieva <modan1679@gmail.com>
2023-06-05 05:22:34 +0300

    update src folder

--- src/imgApi.js
+++ src/imgApi.js
@@ -1,24 +1,59 @@
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '36979931-b9ebd2c49fac6caefdf5e0dc3';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
-const options = {
-  q: 'cat',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  page: 1,
-  per_page: 40,
-};
+const loadMoreButton = document.querySelector('.load-more');
 
-function getImages() {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q='cat'&image_type='photo'&orientation='horizontal'&safesearch=true&page=1&per_page=40`
-  ).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
+class ImgApi {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalHitsResult = 0;
+    this.totalPagesResult = 0;
+    this.per_page = 40;
+  }
+
+  async getImages() {
+    try {
+      const BASE_URL = 'https://pixabay.com/api/';
+      const API_KEY = '36979931-b9ebd2c49fac6caefdf5e0dc3';
+      const image_type = 'photo';
+      const orientation = 'horizontal';
+      const safesearch = true;
+
+      const resp = await axios.get(
+        `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&page=${this.page}&per_page=${this.per_page}`
+      );
+      const result = resp.data;
+      this.totalHitsResult = result.totalHits;
+      this.totalPagesResult = Math.ceil(result.totalHits / this.per_page);
+      this.page += 1;
+      if (this.page > this.totalPagesResult) {
+        // loadMoreButton.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results"
+        );
+      }
+      return result.hits;
+    } catch (err) {
+      console.error(err);
     }
-    return resp.json();
-  });
+  }
+
+  totalHits() {
+    return this.totalHitsResult;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
 
-export { getImages };
+export { ImgApi };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,69 @@
 import Notiflix from 'notiflix';
-import { getImages } from './imgApi';
+import { ImgApi } from './imgApi';
 
 const searchForm = document.querySelector('.search-form');
 const imgGallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
 
+loadMoreButton.style.display = 'none';
+
+const imgApi = new ImgApi();
+
 searchForm.addEventListener('submit', searchImages);
+loadMoreButton.addEventListener('click', loadMoreImg);
 
 function searchImages(evt) {
   evt.preventDefault();
+  loadMoreButton.style.display = 'none';
+  imgGallery.innerHTML = '';
   const { searchQuery } = evt.currentTarget.elements;
-  console.log(searchQuery.value);
+  //   console.log(searchQuery.value);
+  imgApi.query = searchQuery.value;
+  if (imgApi.query.trim() === '') {
+    loadMoreButton.style.display = 'none';
+    return Notiflix.Notify.info('You need to enter certain data');
+  }
+
+  imgApi.resetPage();
+
+  loadMoreButton.style.display = 'none';
+
+  imgApi.getImages().then(hits => {
+    imgGallery.innerHTML = '';
+    addmarkupImageGallery(hits);
+    const totalHits = imgApi.totalHits();
+    if (imgApi.totalHits() < imgApi.per_page) {
+      loadMoreButton.style.display = 'none';
+    } else if (imgApi.totalHits() > 0) {
+      loadMoreButton.style.display = 'block';
+
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+  });
 }
 
-getImages()
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+function loadMoreImg() {
+  loadMoreButton.style.display = 'none';
+  imgApi.getImages().then(hits => {
+    addmarkupImageGallery(hits);
+    const totalHits = imgApi.totalHits();
+    if (imgApi.totalHits() < imgApi.per_page) {
+      loadMoreButton.style.display = 'none';
+    } else {
+      loadMoreButton.style.display = 'block';
+    }
+  });
+}
+
+function addmarkupImageGallery(hits) {
+  imgGallery.insertAdjacentHTML('beforeend', markupImageGallery(hits));
+  if (hits.length === 0) {
+    loadMoreButton.style.display = 'none';
+    Notiflix.Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+}
 
 function markupImageGallery(arr) {
   return arr
@@ -29,19 +77,19 @@ function markupImageGallery(arr) {
         comments,
         downloads,
       }) => `<div class="photo-card">
-        <a href="${largeImageURL}" class="img-link"> < img src = "${webformatURL}" alt = "${tags}" loading = "lazy" data-source="${largeImageURL}" /></a>
-        <div class="info">
+      <img class="image" src = "${webformatURL}" alt = "${tags}" loading = "lazy" width="250"/>
+      <div class="info">
             <p class="info-item">
-                <b>Likes ${likes}</b>
+                <b>Likes: ${likes}</b>
             </p>
             <p class="info-item">
-                <b>Views ${views}</b>
+                <b>Views: ${views}</b>
             </p>
             <p class="info-item">
-                <b>Comments ${comments}</b>
+                <b>Comments: ${comments}</b>
             </p>
             <p class="info-item">
-                <b>Downloads ${downloads}</b>
+                <b>Downloads: ${downloads}</b>
             </p>
         </div>
 </div >`
